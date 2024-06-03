9f8961e8a609b9aafe7f8924a59a2025a18c8aef
Arsenii White <arseniizaripov@yandex.ru>
2024-06-03 13:43:53 +0500

    0.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,39 @@
-"use client";
+import { fetchCars } from "../utils";
+import { HomeProps } from "../types";
+import { fuels, yearsOfProduction } from "../constants";
+import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "../components";
 
-import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
-import { fetchCars } from '@/utils'
-import { fuels, yearsOfProduction } from '@/constants';
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
-export default function Home() {
-  const [allCars, setAllCars] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const [manufacturer, setManufacturer] = useState("");
-  const [model, setModel] = useState("");
-
-  const [fuel, setFuel] = useState("");
-  const [year, setYear] = useState(2023);
-
-  const [limit, setLimit] = useState(10);
-
-  const getCars = async () => {
-    setLoading(true);
-
-    try {
-      const result = await fetchCars({
-        manufacture: manufacturer || '',
-        year: year || 2022,
-        fuel: fuel || '',
-        limit: limit || 10,
-        model: model || '',
-      });
-
-      setAllCars(result);
-    } catch (error) {
-      console.log(error)
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    getCars();
-  }, [fuel, year, limit, manufacturer, model])
-
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
-    <main className="overflow-hidden">
+    <main className='overflow-hidden'>
       <Hero />
-      
+
       <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
           <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
-          <p>Explore the cars you might like</p>
+          <p>Explore out cars you might like</p>
         </div>
 
         <div className='home__filters'>
-          <SearchBar setManufacturer={setManufacturer} setModel={setModel} />
+          <SearchBar />
 
           <div className='home__filter-container'>
-            <CustomFilter setFilter={setFuel} title="fuel" options={fuels} />
-            <CustomFilter setFilter={setYear} title="year" options={yearsOfProduction} />
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
 
-        {allCars.length > 0 ? (
+        {!isDataEmpty ? (
           <section>
             <div className='home__cars-wrapper'>
               {allCars?.map((car) => (
@@ -71,32 +41,18 @@ export default function Home() {
               ))}
             </div>
 
-            {loading && (
-              <div className='mt-16 w-full flex-center'>
-                <Image 
-                  src="/loader.svg"
-                  alt="loader"
-                  width={50}
-                  height={50}
-                  className="object-contain"
-                />
-              </div>
-            )}
-
-            <ShowMore 
-              pageNumber={limit / 10}
-              isNext={limit > allCars.length}
-              setLimit={setLimit}
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
             />
           </section>
-        ): (
+        ) : (
           <div className='home__error-container'>
             <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
-
       </div>
     </main>
-  )
+  );
 }
